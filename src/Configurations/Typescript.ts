0b503adda3a2e901d1b52c0d740b73b09f4baf7f
Matthew Davis <matthew@matthewdavis.io>
2023-01-17 12:08:30 -0500

    bump

--- src/Configurations/Typescript.ts
+++ src/Configurations/Typescript.ts
@@ -3,14 +3,14 @@ export class Typescript {
     public static rules = {
 
         '@typescript-eslint/comma-spacing': 'warn',
-        '@typescript-eslint/padding-line-between-statements': [
-            'error',
-            {
-                'blankLine': 'always',
-                'prev': '*',
-                'next': ['interface', 'type', '*']
-            }
-        ],
+        // '@typescript-eslint/padding-line-between-statements': [
+        //     'error',
+        //     {
+        //         'blankLine': 'always',
+        //         'prev': '*',
+        //         'next': ['interface', 'type', '*']
+        //     }
+        // ],
         '@typescript-eslint/lines-between-class-members': ['error', 'always', {'exceptAfterSingleLine': true}],
         '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
         '@typescript-eslint/type-annotation-spacing': 'warn',
